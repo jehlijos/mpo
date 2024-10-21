232a8c7d79ea8c386764c84002b1439f03f9177b
Josef Jehlička <117917253+jehlijos@users.noreply.github.com>
2024-10-21 11:30:12 +0200

    Add files via upload

---
+++ data/Historickfotografie_14.js
@@ -0,0 +1,11 @@
+var json_Historickfotografie_14 = {
+"type": "FeatureCollection",
+"name": "Historickfotografie_14",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "nazev": 1933, "popis": null, "foto_path": "history\\postavbe1933\\postavbe1933.png" }, "geometry": { "type": "Point", "coordinates": [ 14.426062329863386, 50.093300529905719 ] } },
+{ "type": "Feature", "properties": { "id": "1", "nazev": 1968, "popis": null, "foto_path": "history\\1968\\1968.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.4274092329863386, 50.0928262329863386 ] } }, 
+{ "type": "Feature", "properties": { "id": "1", "nazev": 1977, "popis": null, "foto_path": "history\\1977\\1977.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.4261692329863386, 50.092482329863386 ] } }, 
+{ "type": "Feature", "properties": { "id": "1", "nazev": 1985, "popis": null, "foto_path": "history\\1985\\1985.png" }, "geometry": { "type": "Point", "coordinates": [ 14.4270132329863386, 50.0934182329863386 ] } }
+]
+}
---
+++ data/Novodobfotografie_13.js
@@ -0,0 +1,16 @@
+var json_Novodobfotografie_13 = {
+"type": "FeatureCollection",
+"name": "Novodobfotografie_13",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "nazev": null, "popis": null, "foto_path": "history\\bokhnedasocha/bokhnedasocha.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.426734500258926, 50.092688779973287 ] } },
+{ "type": "Feature", "properties": { "id": "2", "nazev": "Kopule", "popis": "Restaurátorský průzkum odhalil, že dubová okna kopule, stará více než osmdesát let, jsou značně poškozena. V roce 2006 byla část z nich vyměněna za repliky, některá zase nahrazená a u všech provedeno restaurování dle předepsaných technologických postupů. Na závěr se okna ošetřila nátěry tak, aby vynikla přirozená kresba tvrdého dřeva a zachoval se historizující vzhled oken. ", "foto_path": "history\\kopule2006\\kopule2006.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.425941206214137, 50.092949976633264 ] } },
+{ "type": "Feature", "properties": { "id": "3", "nazev": null, "popis": null, "foto_path": "history\\modernicelek\\modernicelek.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.425917349434016, 50.093299382922289 ] } },
+{ "type": "Feature", "properties": { "id": "4", "nazev": null, "popis": null, "foto_path": "history\\ochoz\\ochoz.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.42580631490579, 50.092969575042616 ] } },
+{ "type": "Feature", "properties": { "id": null, "nazev": null, "popis": null, "foto_path": "history\\podkopuli\\podkopuli.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.425922344799194, 50.092851378221447 ] } },
+{ "type": "Feature", "properties": { "id": null, "nazev": null, "popis": null, "foto_path": "history\\rohsedicitypek\\rohsedicitypek.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.425129875436163, 50.093042864999958 ] } },
+{ "type": "Feature", "properties": { "id": null, "nazev": null, "popis": null, "foto_path": "history\\rohstojicihnedejtypek\\rohstojicihnedejtypek.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.426678864118619, 50.093116830838085 ] } },
+{ "type": "Feature", "properties": { "id": "7", "nazev": "Vstup", "popis": null, "foto_path": "history\\vstup\\vstup.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.42590385600848, 50.093141700075577 ] } },
+{ "type": "Feature", "properties": { "id": "8", "nazev": null, "popis": null, "foto_path": "history\\zasedacka\\zasedacka.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.425916952168254, 50.093063446182946 ] } }
+]
+}
