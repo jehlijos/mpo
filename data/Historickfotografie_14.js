bbe76d7847a92bf1a08c642f88de53b4879581c9
Josef Jehlička <117917253+jehlijos@users.noreply.github.com>
2024-10-24 19:22:43 +0200

    Update Historickfotografie_14.js

--- data/Historickfotografie_14.js
+++ data/Historickfotografie_14.js
@@ -4,8 +4,16 @@ var json_Historickfotografie_14 = {
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
 { "type": "Feature", "properties": { "id": "1", "nazev": "1933", "popis": null, "foto_path": "history\\postavbe1933\\postavbe1933.png" }, "geometry": { "type": "Point", "coordinates": [ 14.426062329863386, 50.093300529905719 ] } },
-{ "type": "Feature", "properties": { "id": "1", "nazev": "1968", "popis": null, "foto_path": "history\\1968\\1968.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.4274092329863386, 50.0928262329863386 ] } }, 
-{ "type": "Feature", "properties": { "id": "1", "nazev": "1977", "popis": null, "foto_path": "history\\1977\\1977.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.4261692329863386, 50.092482329863386 ] } }, 
-{ "type": "Feature", "properties": { "id": "1", "nazev": "1985", "popis": null, "foto_path": "history\\1985\\1985.png" }, "geometry": { "type": "Point", "coordinates": [ 14.4270132329863386, 50.0934182329863386 ] } }
+{ "type": "Feature", "properties": { "id": "2", "nazev": "1968", "popis": null, "foto_path": "history\\1968\\1968.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.4274092329863386, 50.0928262329863386 ] } }, 
+{ "type": "Feature", "properties": { "id": "3", "nazev": "1977", "popis": null, "foto_path": "history\\1977\\1977.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.4261692329863386, 50.092482329863386 ] } }, 
+{ "type": "Feature", "properties": { "id": "4", "nazev": "1985", "popis": null, "foto_path": "history\\1985\\1985.png" }, "geometry": { "type": "Point", "coordinates": [ 14.4270132329863386, 50.0934182329863386 ] } }, 
+{ "type": "Feature", "properties": { "id": "5", "nazev": "1985", "popis": null, "foto_path": "history\\revolucni1968\\revolucni1968.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.4275732329863386, 50.0912672329863386 ] } },
+{ "type": "Feature", "properties": { "id": "5", "nazev": "1995", "popis": null, "foto_path": "history\\frant\\frant.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.4205422329863386, 50.091776329863386 ] } }, 
+{ "type": "Feature", "properties": { "id": "6", "nazev": "1910", "popis": null, "foto_path": "history\\1910\\1910.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.4252632329863386, 50.092193329863386 ] } },
+{ "type": "Feature", "properties": { "id": "7", "nazev": "1977", "popis": null, "foto_path": "history\\hast1977\\hast1977.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.4246942329863386, 50.091516329863386 ] } }, 
+{ "type": "Feature", "properties": { "id": "8", "nazev": "1975", "popis": null, "foto_path": "history\\hast1975\\hast1975.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.424871329863386, 50.091159329863386 ] } }, 
+{ "type": "Feature", "properties": { "id": "9", "nazev": "1966", "popis": null, "foto_path": "history\\salvuvnitr\\salvuvnitr.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.424678329863386, 50.092479329863386 ] } }, 
+{ "type": "Feature", "properties": { "id": "10", "nazev": "1947", "popis": null, "foto_path": "history\\salvenku\\salvenku.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.425538329863386,  50.092401329863386 ] } }, 
+{ "type": "Feature", "properties": { "id": "11", "nazev": "1905", "popis": null, "foto_path": "history\\1905\\1905.jpg" }, "geometry": { "type": "Point", "coordinates": [ 14.427523329863386,  50.092287329863386 ] } }
 ]
 }
